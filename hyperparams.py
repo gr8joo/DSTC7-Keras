aecb293a0034189cfe3f7cffc854b61457bd4f81
gr8joo <hjhwang@ai.kaist.ac.kr>
2018-09-11 15:26:30 +0900

    Codes for external knowledges are added

--- hyperparams.py
+++ hyperparams.py
@@ -2,27 +2,30 @@ class hyper_parameters:
     def __init__(self):
 
         # General parameters
-        self.vocab_size = 209825#4900#321618
+        self.vocab_size =  326385#209825 ubuntu#4900 advising#321618
         self.num_utterance_options = 100
+        self.num_knowledge_options = 40
         
         self.max_seq_len = 230#90
         self.num_uttr_context = 42
 
-        self.max_context_len = 1250#400#
-        self.max_utterance_len = 230#90#
+        self.max_context_len = 600#1250 ubuntu #400 advising
+        self.max_utterance_len = 140#230 ubuntu #90 advising
+        self.max_knowledge_len = 150
         self.max_profile_len = 24
+
 
 
         #### Model speicific parameters ####
         # Dual encoder
-        self.de_rnn_dim = 100#128#256#300
+        self.de_rnn_dim = 128#256#300
         self.de_embedding_dim = 300
 
         # Memory network(n2n)
         self.memn2n_rnn_dim = 128#64
         self.memn2n_embedding_dim = 300
         self.memn2n_drop_rate = 0.3
-        self.hops = 10
+        self.hops = 3
     
         # CNN_1D
         self.cnn_rnn_dim = 64
@@ -50,26 +53,26 @@ class hyper_parameters:
 
         ########## Data Variants ##########
         # Locations of digitized sentences of training/valdiation data
-        self.train_context_path = 'train_data/train_ubuntu/train_context.npy'#  'valid_data/valid_context.npy'#
-        self.train_context_len_path = 'train_data/train_ubuntu/train_context_len.npy'# 'valid_data/valid_context_len.npy'#
-        self.train_context_speaker_path = 'train_data/train_ubuntu/train_context_speaker.npy'# 'train_data/context_speaker.train'#
+        self.train_context_path = 'train_data/train_ubuntu_uKB/train_context.npy'
+        self.train_context_len_path = 'train_data/train_ubuntu_uKB/train_context_len.npy'
+        self.train_context_speaker_path = 'train_data/train_ubuntu_uKB/train_context_speaker.npy'
 
-        self.train_target_path = 'train_data/train_ubuntu/train_target.npy'# 'valid_data/valid_advising/valid_target.npy'#
+        self.train_target_path = 'train_data/train_ubuntu/train_target.npy'
         
-        self.train_options_path = 'train_data/train_ubuntu/train_options.npy'# 'valid_data/valid_options.npy'#
-        self.train_options_len_path = 'train_data/train_ubuntu/train_options_len.npy'# 'valid_data/valid_options_len.npy'#
+        self.train_options_path = 'train_data/train_ubuntu_uKB/train_options.npy'
+        self.train_options_len_path = 'train_data/train_ubuntu_uKB/train_options_len.npy'
 
         self.train_profile_path = 'train_data/train_ubuntu/train_profile.npy'
 
 
-        self.valid_context_path = 'valid_data/valid_ubuntu/valid_context.npy'
-        self.valid_context_len_path = 'valid_data/valid_ubuntu/valid_context_len.npy'
-        self.valid_context_speaker_path = 'valid_data/valid_ubuntu/valid_context_speaker.npy'# 'valid_data/context_speaker.valid'#
+        self.valid_context_path = 'valid_data/valid_ubuntu_uKB/valid_context.npy'
+        self.valid_context_len_path = 'valid_data/valid_ubuntu_uKB/valid_context_len.npy'
+        self.valid_context_speaker_path = 'valid_data/valid_ubuntu_uKB/valid_context_speaker.npy'
 
         self.valid_target_path = 'valid_data/valid_ubuntu/valid_target.npy'
 
-        self.valid_options_path = 'valid_data/valid_ubuntu/valid_options.npy'
-        self.valid_options_len_path = 'valid_data/valid_ubuntu/valid_options_len.npy'
+        self.valid_options_path = 'valid_data/valid_ubuntu_uKB/valid_options.npy'
+        self.valid_options_len_path = 'valid_data/valid_ubuntu_uKB/valid_options_len.npy'
 
         self.valid_profile_path = 'valid_data/valid_ubuntu/valid_profile.npy'
         
